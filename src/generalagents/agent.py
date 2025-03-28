6cd26aff869a6af0b0b819dd71706579a51e555a
Paulius Sarka <paulius.sarka@gmail.com>
2025-03-28 17:43:27 +0100

    Previous actions are now a sequence

--- src/generalagents/agent.py
+++ src/generalagents/agent.py
@@ -9,14 +9,23 @@ from generalagents.action import Action
 
 
 class Session:
-    def __init__(self, model: str, api_key: str, instruction: str, temperature: float):
+    def __init__(
+        self,
+        model: str,
+        api_key: str,
+        instruction: str,
+        temperature: float,
+        max_previous_actions: int,
+    ):
+        """"""
         self.model = model
         self.instruction = instruction
+        self.max_previous_actions = max_previous_actions
         self.client = httpx.Client(
             base_url="https://api.generalagents.com",
             headers={"Authorization": f"Bearer {api_key}"},
         )
-        self.previous_action = None
+        self.previous_actions = []
         self.temperature = temperature
 
     def plan(self, observation: Image.Image) -> Action:
@@ -28,7 +37,7 @@ class Session:
             "model": self.model,
             "instruction": self.instruction,
             "image_url": image_url,
-            "previous_action": self.previous_action,
+            "previous_actions": self.previous_actions[-self.max_previous_actions :],
             "temperature": self.temperature,
         }
 
@@ -36,16 +45,24 @@ class Session:
         res.raise_for_status()
 
         action = res.json()["action"]
-        self.previous_action = action
+        self.previous_actions.append(action)
         print(f"Received action {action}")
         return cattrs.structure(action, Action)  # pyright: ignore [reportArgumentType] https://peps.python.org/pep-0747
 
 
 class Agent:
-    def __init__(self, model: str, api_key: str, temperature: float = 0.3):
+    def __init__(
+        self,
+        model: str,
+        api_key: str,
+        temperature: float = 0.3,
+        max_previous_actions: int = 20,
+    ):
+        """"""
         self.model = model
         self.api_key = api_key
         self.temperature = temperature
+        self.max_previous_actions = max_previous_actions
 
     def start(self, instruction: str) -> Session:
         return Session(
@@ -53,4 +70,5 @@ class Agent:
             api_key=self.api_key,
             instruction=instruction,
             temperature=self.temperature,
+            max_previous_actions=self.max_previous_actions,
         )
